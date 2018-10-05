59297135358bd222fc7303d804cb488473e915e8
Mayukh <04mayukh@gmail.com>
2018-10-06 00:20:36 +0530

    Public Folder added

--- src/server.js
+++ src/server.js
@@ -18,6 +18,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 let uri =
   "mongodb://" +
   process.env.MONGO_USER +
